ee85bc405f09332a9c073bb7a02a0f37cac9f39a
kanha-sydaphasavanh <k.sydaphasavanh@laposte.net>
2022-01-13 22:33:56 +0100

    main

---
+++ src/main/java/fr/web/recipy/RecipyApplication.java
@@ -0,0 +1,13 @@
+package fr.web.recipy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipyApplication.class, args);
+    }
+
+}
