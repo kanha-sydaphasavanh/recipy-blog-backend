4c14cb82a6b24e2c245f7226da5d832789920e50
Kanha <k.sydaphasavanh@gmail.com>
2022-08-09 12:05:54 +0200

    add methods

--- src/main/java/fr/web/recipy/services/RecipeServiceImpl.java
+++ src/main/java/fr/web/recipy/services/RecipeServiceImpl.java
@@ -1,16 +1,20 @@
 package fr.web.recipy.services;
 
 import fr.web.recipy.dto.RecipeDto;
+import fr.web.recipy.dto.UserDto;
 import fr.web.recipy.entities.Recipe;
 import fr.web.recipy.entities.User;
+import fr.web.recipy.entities.enums.Category;
+import fr.web.recipy.entities.enums.Difficulty;
 import fr.web.recipy.repositories.RecipeRepository;
-import fr.web.recipy.repositories.UserRepository;
-import fr.web.recipy.tools.DtoMapper;
+import fr.web.recipy.tools.DtoTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,15 +23,18 @@ import java.util.Optional;
 public class RecipeServiceImpl implements RecipeService {
     @Autowired
     RecipeRepository recipeRepository;
+    //    @Autowired
+//    UserRepository userRepository;
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
+
 
     @Override
     public List<RecipeDto> findAll() {
         List<Recipe> recipes = recipeRepository.findAll();
         List<RecipeDto> recipeDtoList = new ArrayList<>();
         for (Recipe r : recipes) {
-            RecipeDto recipeDto = DtoMapper.convert(r, RecipeDto.class);
+            RecipeDto recipeDto = DtoTool.convert(r, RecipeDto.class);
             recipeDtoList.add(recipeDto);
         }
         return recipeDtoList;
@@ -38,30 +45,34 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipe = recipeRepository.findById(id);
 
         if (recipe.isPresent()) {
-//            List<Ingredient> ingredientList = recipe.get().getIngredients();
-//            List<IngredientDto> ingredientDtoList = new ArrayList<>();
-//            for (Ingredient ingredient : ingredientList) {
-//                IngredientDto ingredientDto = DtoMapper.convert(ingredient, IngredientDto.class);
-//                ingredientDtoList.add(ingredientDto);
-//            }
-//            recipeDto.setIngredientsDto(ingredientDtoList);
 
             List<String> ingredients = recipe.get().getIngredients();
             List<String> steps = recipe.get().getSteps();
 
-            RecipeDto recipeDto = DtoMapper.convert(recipe.get(), RecipeDto.class);
+            RecipeDto recipeDto = DtoTool.convert(recipe.get(), RecipeDto.class);
             recipeDto.setIngredientsDto(ingredients);
             recipeDto.setStepsDto(steps);
+
+
+            User user = recipe.get().getAuthor();
+            UserDto userDto = DtoTool.convert(user, UserDto.class);
+            recipeDto.setAuthorDto(userDto);
+
+
             return recipeDto;
         }
         return null;
     }
 
     @Override
-    public RecipeDto saveOrUpdate(RecipeDto recipeDto) {
-        Recipe recipe = DtoMapper.convert(recipeDto, Recipe.class);
+    public RecipeDto saveOrUpdate(RecipeDto recipeDto) throws Exception {
+        Recipe recipe = DtoTool.convert(recipeDto, Recipe.class);
+        if (recipe.getAuthor() == null)
+            throw new Exception("aucun autheur de la recette");
+
         recipe = recipeRepository.saveAndFlush(recipe);
-        return DtoMapper.convert(recipe, RecipeDto.class);
+        return DtoTool.convert(recipe, RecipeDto.class);
+//        return mapper.RecipeToRecipeDto(recipe);
     }
 
     @Override
@@ -74,5 +85,49 @@ public class RecipeServiceImpl implements RecipeService {
             recipe.get().setSteps(null);
         }
         recipeRepository.deleteById(id);
+    }
+
+    @Override
+    public void insertExample() {
+        Recipe recipe = new Recipe();
+        try {
+            recipe.setTitle("Tomate mozarella");
+            recipe.setCategory(Category.ENTREE);
+            recipe.setDifficulty(Difficulty.FACILE);
+            recipe.setNbPerson(2);
+            recipe.setIngredients(Arrays.asList("Tomates", "Mozarella", "Huile d'olive"));
+            recipe.setSteps(Arrays.asList(
+                    "Coupez les tomates et la mozarella en rondelles",
+                    "Dans une assiete, alternez entre rondelle de tomate et de mozarella",
+                    "Ajouter un filet d'huile d'olive"));
+            recipe.setCookingTime(LocalTime.of(0, 20));
+            recipe.setPrice(15);
+
+            // Convertion Recipe en RecipeDto
+            RecipeDto recipeDto = DtoTool.convert(recipe, RecipeDto.class);
+
+            // Definis autheur en le recup dans la BDD avc findById (Si pas de user en BDD => lancer jeu de test du User)
+            recipeDto.setAuthorDto(userService.findById(1));
+
+            // Recupere les ingredients puis convertis en DTO
+            List<String> ingredients = recipe.getIngredients();
+            List<String> ingredientsDto = new ArrayList<>();
+            for (String ingredient : ingredients) {
+                ingredientsDto.add(ingredient);
+            }
+
+            // Recupere les etapes puis convertis en DTO
+            List<String> steps = recipe.getSteps();
+            List<String> stepsDto = new ArrayList<>();
+            for (String step : steps) {
+                stepsDto.add(step);
+            }
+
+            recipeDto.setIngredientsDto(ingredientsDto); // Ajoute les ingredients
+            recipeDto.setStepsDto(stepsDto); // Ajoute les steps
+            saveOrUpdate(recipeDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
