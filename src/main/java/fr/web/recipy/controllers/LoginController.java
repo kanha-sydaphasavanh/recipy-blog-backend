1efa50d0a39b2ef97059d2545df765e1454b2963
Kanha <k.sydaphasavanh@gmail.com>
2022-08-10 09:47:05 +0200

    login controller get token return response when ok

--- src/main/java/fr/web/recipy/controllers/LoginController.java
+++ src/main/java/fr/web/recipy/controllers/LoginController.java
@@ -1,22 +1,30 @@
 package fr.web.recipy.controllers;
 
 import fr.web.recipy.dto.Login;
+import fr.web.recipy.dto.LoginResponse;
 import fr.web.recipy.dto.UserDto;
 import fr.web.recipy.services.UserService;
 import fr.web.recipy.tools.HashTool;
+import fr.web.recipy.tools.TokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/authenticate")
 public class LoginController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private TokenGenerator tokenGenerator;
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<?> login(@RequestBody Login login) {
@@ -24,12 +32,20 @@ public class LoginController {
 
         if (userDto != null) {
             boolean checked = HashTool.checkPassword(login.getPassword(), userDto.getPassword());
-            if (checked)
-            // TODO : return token if checked
-                return ResponseEntity.status(HttpStatus.OK).body("AUTHENTICATE OK");
-            else
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("WRONG PASSWORD");
+            if (!checked)
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new LoginResponse("WRONG PASSWORD"));
+
+            Map<String, Object> claim = new HashMap<>();
+            claim.put("id", userDto.getId());
+            claim.put("email", userDto.getEmail());
+            claim.put("firstName", userDto.getFstName());
+            claim.put("lastName", userDto.getLastName());
+            
+            String token = tokenGenerator.doGenerateToken(claim, userDto.getEmail());
+
+            return new ResponseEntity<>(new LoginResponse(token), HttpStatus.OK);
+
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("NO USER");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new LoginResponse("NO USER"));
     }
 }
