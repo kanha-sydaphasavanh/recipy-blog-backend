64fe83fb007037de5a3a6f46817aae669b779738
kanha-sydaphasavanh <k.sydaphasavanh@laposte.net>
2022-08-11 14:39:07 +0200

    add interceptor to main

--- src/main/java/fr/web/recipy/RecipyApplication.java
+++ src/main/java/fr/web/recipy/RecipyApplication.java
@@ -1,7 +1,13 @@
 package fr.web.recipy;
 
+import fr.web.recipy.interceptor.TokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RecipyApplication {
@@ -10,4 +16,23 @@ public class RecipyApplication {
         SpringApplication.run(RecipyApplication.class, args);
     }
 
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
+
+    @Bean
+    public WebMvcConfigurer mvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/swagger-ui/index.html").allowedMethods("POST", "PUT").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*", "GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowCredentials(false).maxAge(3600);
+            }
+
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(tokenInterceptor);
+            }
+        };
+
+    }
 }
