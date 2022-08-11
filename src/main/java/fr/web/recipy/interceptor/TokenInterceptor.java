65e39ec1d7c28635b0baf70326c6fbb0ea881df0
kanha-sydaphasavanh <k.sydaphasavanh@laposte.net>
2022-08-11 14:38:19 +0200

    TokenInterceptor

--- src/main/java/fr/web/recipy/interceptor/TokenInterceptor.java
+++ src/main/java/fr/web/recipy/interceptor/TokenInterceptor.java
@@ -12,38 +12,38 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class TokenInterceptor implements HandlerInterceptor {
 
-	@Autowired
-	private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		System.out.println(">>>>>> inside Token Interceptor...");
-		System.out.println("URI =" + request.getRequestURI());
-		System.out.println("Header (authorization) :" + request.getHeader("Authorization"));
-		if (!request.getMethod().equals("OPTIONS")) {
-			if (!request.getRequestURI().equals("/authenticate")
-					&& !request.getRequestURI().equals("/insert-example")) {
-				String headerAuth = request.getHeader("Authorization");
-				if (headerAuth == null || headerAuth.trim().equals("") || headerAuth.length() < 7) {
-					throw new TokenException("Erreur : jeton absent ou invalide !");
-				}
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        System.out.println("======== inside Token Interceptor ========");
+//		System.out.println("URI =" + request.getRequestURI());k
+//		System.out.println("Header (authorization) :" + request.getHeader("Authorization"));
+        if (!request.getMethod().equals("OPTIONS")) {
+            if (!request.getRequestURI().equals("/authenticate")) {
+                String headerAuth = request.getHeader("Authorization");
+                if (headerAuth == null || headerAuth.trim().equals("") || headerAuth.length() < 7) {
+                    throw new TokenException("Erreur : jeton absent ou invalide !");
+                }
 
-				String token = headerAuth.substring(7);
-				// validation le token et extraire les infos
-				if (jwtTokenUtil.isTokenExpired(token))
-					throw new TokenException("Erreur : jeton expiré !");
+                String token = headerAuth.substring(7);
+                // validation le token et extraire les infos
+                if (jwtTokenUtil.isTokenExpired(token))
+                    throw new TokenException("Erreur : jeton expiré !");
 
-				String email = jwtTokenUtil.getUsernameFromToken(token);
-				if (!TokenSaver.tokensByEmail.containsKey(email) || !TokenSaver.tokensByEmail.get(email).equals(token))
-					throw new TokenException("Erreur : jeton non reconnu !");
+                String email = jwtTokenUtil.getUsernameFromToken(token);
+                System.out.println("======== EMAIL : " + email);
+                if (!TokenSaver.tokensByEmail.containsKey(email) || !TokenSaver.tokensByEmail.get(email).equals(token))
+                    throw new TokenException("Erreur : jeton non reconnu !");
 
-				// TODO autres extractions du jeton ou autres traitements
+                // TODO autres extractions du jeton ou autres traitements
 
-			}
-		}
-		return true;
-	}
+            }
+        }
+        return true;
+    }
 
 }
 
